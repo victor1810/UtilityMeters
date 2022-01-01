57d1b7980dece9caf1a6f805aae8d95adda04739
victor1810 <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>
2022-01-01 10:55:22 +0300

    Added a working flashlight button

--- app/src/main/java/com/victor/utilitymeters/MainActivity.kt
+++ app/src/main/java/com/victor/utilitymeters/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.victor.utilitymeters
 
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ToggleButton
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+   private lateinit var cameraManager: CameraManager
+   private lateinit var cameraId: String
+   private lateinit var toggleButton: ToggleButton
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_main)
+      title = "KotlinApp"
+      val isFlashAvailable = applicationContext.packageManager
+      .hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)
+      if (!isFlashAvailable) {
+         showNoFlashError()
+      }
+      cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+      try {
+         cameraId = cameraManager.cameraIdList[0]
+      } catch (e: CameraAccessException) {
+         e.printStackTrace()
+      }
+      toggleButton = findViewById(R.id.flashlightButton)
+      toggleButton.setOnCheckedChangeListener { _, isChecked -> switchFlashLight(isChecked) }
+   }
+   private fun showNoFlashError() {
+      val alert = AlertDialog.Builder(this)
+      .create()
+      alert.setTitle("Oops!")
+      alert.setMessage("Flash not available in this device...")
+      alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK") { _, _ -> finish() }
+      alert.show()
+   }
+   private fun switchFlashLight(status: Boolean) {
+      try {
+         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            cameraManager.setTorchMode(cameraId, status)
+         }
+      } catch (e: CameraAccessException) {
+         e.printStackTrace()
+      }
+   }
 }
